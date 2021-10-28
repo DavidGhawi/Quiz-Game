020e20ef4b9f1eb2578f184f3f77bc3df1b3579e
Chris Nickels <spacebot9@gmail.com>
2021-10-28 12:31:00 +0100

    added soem demo questions and answers for testing purposes

--- server.py
+++ server.py
@@ -9,7 +9,30 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 
 
+@app.route(/inputed_data)
 
+
+submitted_questions = {
+    1: "What color is a london bus?"
+    2: "What speed does a snake flap its wings?"
+}
+
+submitted_answers = {
+    1: "red"
+    2: "23 speed"
+}
+
+approved_questions = {
+    1: "What color is a coke can?",
+    2: "What is the capital city of Wales?",
+    3: "How many fingers does a human have?"
+}
+
+approved_answers = {
+    1:"red",
+    2 :"Cardiff",
+    3: "10"
+}
 
 
 
