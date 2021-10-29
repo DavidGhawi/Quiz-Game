5aee05a59f8950cf37cb7a85f2adede691438b73
David Ghawi <ghawid@cardiff.ac.uk>
2021-10-29 09:19:26 +0100

    Davids Server file

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 
 submitted_questions = {
-    1: "What color is a london bus?"
+    1: "What color is a london bus?",
     2: "What speed does a snake flap its wings?"
 }
 
 submitted_answers = {
-    1: "red"
+    1: "red",
     2: "23 speed"
 }
 
@@ -25,16 +25,38 @@ approved_questions = {
 }
 
 approved_answers = {
-    1:"red",
-    2 :"Cardiff",
+    1: "red",
+    2: "Cardiff",
     3: "10"
 }
+
+global correctcounter
 
 
 # ADD ALL APP ROUTES HERE
 
+@app.route("/approved_questions", methods=["GET"])
+def returnDir():
+    if request.method == 'GET':
+        print("getting directory.")
+        return json.dumps(approved_questions);
 
+@app.route("/Useranswer", methods=["post"])
+def checkans():
+    global correctcounter
+    print("Checking answer")
+    if request.method == "POST":
+        answer = request.form["answer"]
+        print (answer + "IT WORKED")
+        if answer == approved_answers[1]:
+            correctcounter += 1
+    return answer
 
+@app.route("/LoadQuestions", methods=["GET"])
+def returnQuestions():
+    if request.method == 'GET':
+        print("getting questions")
+        return json.dumps(approved_questions);
 
 
 
